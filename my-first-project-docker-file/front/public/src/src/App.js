c2babfa2d37871d975fa73b701020432b5edcdc9
Eduard8798 <eduard9999997@gmail.com>
2024-10-31 23:23:37 +0200

    Docker_compos

---
+++ my-first-project-docker-file/front/public/src/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import JwtExample from "./JwtExample";
+
+function App() {
+  return (
+      <div>
+      <JwtExample/>
+    </div>
+
+  );
+}
+
+export default App;
